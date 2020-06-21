b152cd27e27ae81143f3c928ff2b8f90d832b31e
Jason Hur <jasonhur@Jasons-MacBook-Pro.local>
2020-06-21 12:47:49 -0700

    add model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+SIZES = (
+    ('X', 'Extra Small'),
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('I', 'Extra Large'),
+)
+
+class Product(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.IntegerField()
+  description = models.TextField(max_length=250)
+  size = models.CharField(
+    max_length=1,
+    choices=SIZES,
+    default=SIZES[0][0]
+  )
+  quantity = models.IntegerField()
+  category = models.TextField(max_length=100)
+
